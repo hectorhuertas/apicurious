5c68ea8e79b0bd99d064d269ef2d9a66e2b6c53e
Hector Huertas <hectorhuertas@gmail.com>
2016-02-17 11:20:46 -0700

    Setup the headers at initialize

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -3,9 +3,12 @@ class GithubService
 
   def initialize(current_user)
     @conn = Faraday.new(url: 'https://api.github.com')
+    conn.headers = {
+      'Accept' => 'application/vnd.github.v3+json',
+      'Authorization'=>"token #{current_user.token}" }
   end
 
   def user
-    @conn.get("/user", {}, {'Authorization'=>"token #{current_user.token}"})
+    @conn.get("/user")
   end
 end
