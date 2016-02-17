eba293ccc78384f9b7af5dab6bfca6d9f1c0c1c2
Hector Huertas <hectorhuertas@gmail.com>
2016-02-17 13:06:47 -0700

    Setup factory girl, mocha and capybara, and redirect user to dashboard

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://rubygems.org'
 
+gem 'capybara'
+gem 'mocha'
+gem 'factory_girl_rails'
 gem 'pry'
 # gem 'pry-byebug'
 gem 'omniauth-github'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 class PagesController < ApplicationController
   def login
+    redirect_to :dashboard if current_user
   end
 
   def dashboard
   end
 
   def supertest
-    pry
   end
 end
