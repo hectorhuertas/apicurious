4dab29c5de88304a7c6750bfd30145531f144d64
Hector Huertas <hectorhuertas@gmail.com>
2016-02-18 14:33:18 -0700

    Style person partial

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,10 +5,15 @@ class PagesController < ApplicationController
 
   def dashboard
     git = GithubService.new(current_user)
-    @user = git.user
-    @starred = git.starred
-    @contributions = git.contributions
-    @commit_history = git.commit_history
-    @repos = git.repos
+    # @user = git.user
+    @user = {'followers'=> 5, 'following'=>8}
+    # @starred = git.starred
+    @starred = 9
+    # @contributions = git.contributions
+    @contributions = {}
+    # @commit_history = git.commit_history
+    @commit_history = {}
+    # @repos = git.repos
+    @repos = {}
   end
 end
